a24e7871f7b2807aace548f90e53fe3f31d46e26
oli <pozortulsi@gmail.com>
2026-03-08 15:33:17 +0100

    ex4 ongoing

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python3
-Build complex systems with dictionaries
+
+import sys
 
 def function() -> None:
     pass
 
 
 def main() -> None:
-    pass
+    
+    print("=== Inventory System Analysis ===")
+
+    inventory = {}
+    try:
+        for item in sys.argv[1:]:
+            name, qty = item.split(":")
+            qty = int(qty)
+            current = inventory.get(name, 0)
+            inventory.update({name:qty+current})
+
+    except ValueError:
+        print(f"Error with {name}: {qty} is not valid int. Skipping this input")
+
+    total_items = 0
+    for value in inventory.values():
+        total_items += value
+    
+    print(f"Total items in inventory: {total_items}")
+
+    item_types = len(inventory)
+    
+    print(f"Unique item types: {item_types}\n")
+    
+    print("=== Current Inventory ===")
+    for name, qtu in inventory.items():
+        print(f"{name}: {qty} units")
+    print(inventory)
+
+        
 
 
 if __name__ == "__main__":
